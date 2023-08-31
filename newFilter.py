bd8b64ff0daba3469ae26c880cad2b8d05629040
Logan Bolton <loganbolton101@gmail.com>
2023-08-30 22:18:21 -0500

    broken multiplier

--- newFilter.py
+++ newFilter.py
@@ -2,10 +2,11 @@ import cv2
 import numpy as np
 import random
 
+multiplier = 0.01
 def apply_rainbow_effect(frame):
     # Convert frame to float32 to prevent overflow
     frame = np.float32(frame)
-    randomMultiplier = random.uniform(0.1, 1.0)
+    # randomMultiplier = random.uniform(0.1, 1.0)
 
     # Get height and width of the frame
     h, w, _ = frame.shape
@@ -15,9 +16,9 @@ def apply_rainbow_effect(frame):
     y = np.linspace(-2*np.pi, 2*np.pi, h).reshape(-1, 1)
 
     # Create sinusoidal functions to create the rainbow effect
-    r_effect = np.sin(y + x) * 45 * randomMultiplier
-    g_effect = np.sin(y + x + 2*np.pi/3) * 45 * randomMultiplier
-    b_effect = np.sin(y + x + 4*np.pi/3) * 45 * randomMultiplier
+    r_effect = np.sin(y + x) * 45*multiplier
+    g_effect = np.sin(y + x + 2*np.pi/3) * 45*multiplier
+    b_effect = np.sin(y + x + 4*np.pi/3) * 45*multiplier
 
     # Add the effects to the original frame values
     frame[..., 0] += b_effect
